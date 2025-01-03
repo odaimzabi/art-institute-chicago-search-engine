7a1ebf49cf00eb641b41bb23a70443fbe4948de8
Andreas Greger <andreas.greger@veroDigit.de>
2025-01-03 10:09:21 +0100

    minor codeFixes & new Controller for userInformation

--- server/src/main/java/com/odaimzabi/art_institute_chicago_search_engine/services/ArtworkService.java
+++ server/src/main/java/com/odaimzabi/art_institute_chicago_search_engine/services/ArtworkService.java
@@ -4,6 +4,7 @@ import com.odaimzabi.art_institute_chicago_search_engine.entities.Artwork;
 import com.odaimzabi.art_institute_chicago_search_engine.mappers.ArtworkEntityMapper;
 import com.odaimzabi.art_institute_chicago_search_engine.utils.FileReader;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
@@ -13,15 +14,21 @@ import java.util.ArrayList;
 @Service
 @RequiredArgsConstructor
 public class ArtworkService {
-  private final FileReader fileReader;
-  private final ArtworkEntityMapper artworkEntityMapper;
+
+  @Autowired
+  private FileReader fileReader;
+
+  @Autowired
+  private ArtworkEntityMapper artworkEntityMapper;
 
   public ArrayList<Artwork> fetchArtworksByQuery(String query) throws IOException {
     ArrayList<Artwork> artworks = new ArrayList<>();
-    ArrayList<File> jsonFiles = this.fileReader.readDirectory("/artworks", ".json");
+    ArrayList<File> jsonFiles = fileReader.readDirectory("/artworks", ".json");
 
+
+    // todo which other way to solve this?
     for (File jsonFile : jsonFiles) {
-      Artwork artwork = this.artworkEntityMapper.mapJsonToArtwork(jsonFile);
+      Artwork artwork = artworkEntityMapper.mapJsonToArtwork(jsonFile);
 
       if (artwork.getTitle().toLowerCase().contains(query)) {
         artworks.add(artwork);
