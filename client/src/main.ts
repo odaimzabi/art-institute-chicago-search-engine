825fc6a9408f5d4b132f3f67afbabc5994726f7b
Andreas Greger <andreas.greger@veroDigit.de>
2025-01-03 10:30:14 +0100

    improved the FE

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,4 +3,8 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+  .catch((err) => console.error(err))
+  .then(() => {
+    const someValue = appConfig.startupInformation.value;
+    console.log(someValue);
+  });
